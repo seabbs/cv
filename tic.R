de6bc7050facab8ead398634cfbc43b20df54cff
Sam Abbott <contact@samabbott.co.uk>
2019-10-02 14:51:49 +0100

    tic render location

--- tic.R
+++ tic.R
@@ -10,7 +10,7 @@ get_stage("install") %>%
 if (ci_on_travis()) {
   get_stage("deploy") %>%
     add_step(step_setup_push_deploy(path = "cv", branch = "gh-pages")) %>%
-    add_code_step(rmarkdown::render(here::here("cv", "cv.Rmd"), output_dir = "."))
+    add_code_step(rmarkdown::render(here::here("cv", "cv.Rmd")))
   
   if (ci_has_env("id_rsa")) {
     get_stage("before_deploy") %>%
